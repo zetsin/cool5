8a54cacb5dafb431b780fdd2c7f5faddf87e60d4
Jianru Lin <jianru.lin@gmail.com>
2015-11-13 23:23:46 +0800

    初步做了下 auth 模块

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,45 @@
 var config = require('./config')
 
-// return: {ok: <boolean>, forward: <boolean>}
-exports.exec = function(header) {
-	return {
-		ok: true,
-		forward: false
+// if auth is not enabled, return ok and forward nothing
+if (!config.get('auth.enabled')) {
+	exports.exec = function(header) {
+		return {
+			ok: true,
+			forward: null
+		}
 	}
+}
+else if (config.get('auth.mode') === '') {
+
+
+}
+
+
+// return: {ok: <boolean>, forward: <gpp-header>}
+exports.exec = function(header) {
+
+	var ok = false
+	if (!config.get('auth.verify')) {
+		ok = true
+	}
+	else {
+		// TODO
+		ok = false
+	}
+
+	var forward = null
+	if (config.get('auth.forward')) {
+		forward = {
+			auth: header.auth
+		}
+	}
+
+	var ret = {
+		ok: ok,
+		forward: forward
+	}
+
+	log.info('[auth] ok=${ok}, forward=${forward|json}', ret)
+
+	return ret
 }
