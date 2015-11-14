997984d57813330b43ff3ae249a6ec0ad8d1dd19
Jianru Lin <jianru.lin@gmail.com>
2015-11-15 01:06:44 +0800

    补充了 fsync 一部分代码

--- src/fsync.js
+++ src/fsync.js
@@ -1,9 +1,29 @@
 var config = require('./config')
+var log = require('./log')
 
-exports.add_sync_target = function(name) {
-
+if (!config.get('fsync.enabled')) {
+	// not enabled? do nothing
+	exports.sync = function(name, change_cb) {}
 }
+else {
+	var mode = config.get('fsync.mode')
+	if (mode !== 'poll') {
+		// only poll mode is supported yet
+		log.error('unknown fsync.mode: ' + mode)
+		process.exit(1)
+	}
 
-exports.on_sync_target_changed = function(name, cb) {
-	
+	var poll_interval = config.get('fsync.poll.interval')
+	// support: second, minute, hour
+	// eg.
+	// 15.5s
+	// 0.75m
+	// 3.2h
+	if (!/^\d+(\.\d+)[s|m|h]$/i.test(poll_interval)) {
+		log.error('invalid fsync.poll_interval: ' + poll_interval)
+		process.exit(1)
+	}
+
+	var poll_url
+	// TODO
 }
